1465673695ce5b98b43d6de53d1c869d5b760630
abretelle <abretelle@gamned.com>
2023-10-24 18:47:15 +0200

    Add all items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,20 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+
+// export class AppComponent {
+//   title = 'la-brasserie';
+// }
+
+// app.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +22,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'la-brasserie';
+  allBeers: any[] = []; // Propriété pour stocker toutes les bières
+
+  constructor(private httpService: HttpService) { }
+
+  ngOnInit() {
+    this.httpService.fetchDataFromUrl().then(response => {
+      console.log(response.data);
+      // Affectez toutes les bières à la propriété allBeers
+      this.allBeers = response.data;
+    }).catch(error => {
+      console.error(error);
+    });
+  }
 }
+
---
+++ src/app/http.service.ts
@@ -0,0 +1,17 @@
+// http.service.ts
+
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  private apiUrl = 'https://api.punkapi.com/v2/beers';
+
+  constructor() { }
+
+  fetchDataFromUrl() {
+    return axios.get(this.apiUrl);
+  }
+}
