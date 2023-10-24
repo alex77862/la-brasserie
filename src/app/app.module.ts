4f369c2c441aafe677f09777f50b8cf35f2a795b
abretelle <abretelle@gamned.com>
2023-10-24 15:04:49 +0200

    add first component (navbar)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     MatFormFieldModule,
     MatInputModule,
-    BrowserAnimationsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
