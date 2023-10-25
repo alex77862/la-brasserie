81fcde5a31fe23b56a1ab11fb3e49dbc796f2b36
abretelle <abretelle@gamned.com>
2023-10-26 00:47:15 +0200

    search-bar works

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,25 +1,35 @@
 // search-bar.component.ts
-import { Component } from '@angular/core';
+
+import { Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angular/core';
 import { SearchService } from '../search.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-
 export class SearchBarComponent {
   searchTerm: string = '';
   results: any[] = [];
+  showDropdown: boolean = false;
 
-  constructor(private searchService: SearchService) {}
+  @ViewChild('dropdown') dropdown!: ElementRef;
+
+  constructor(private searchService: SearchService, private renderer: Renderer2) {}
 
   onSearch() {
     this.searchService.getAllBeers()
       .then(data => {
         // Filtrer les bières en fonction du terme de recherche
         this.results = data.filter((beer: any) => beer.name.toLowerCase().includes(this.searchTerm.toLowerCase()));
+        this.showDropdown = this.results.length > 0; // Affiche le dropdown si des résultats sont trouvés
       });
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: any) {
+    if (this.dropdown && !this.dropdown.nativeElement.contains(event.target)) {
+      this.showDropdown = false; // Masque le dropdown si vous cliquez à l'extérieur
+    }
   }
 }
